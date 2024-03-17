4fb2a171f84b36b8524efc796144b64725626118
Amin Saboohi <aminsaboohi.eepd.gmail.com>
2024-03-17 19:39:27 +0330

    add users and phone book app as apps to project

--- phonebook_blog/settings.py
+++ phonebook_blog/settings.py
@@ -29,6 +29,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # TrustedApps
+    'rest_framework',
+    'django_filters',
+    'rest_framework_simplejwt',
+
+    # My Apss
+    'phone_book_app.apps.PhoneBookAppConfig',
+    'users.apps.UsersConfig',
+
+    # Third-party Apps
 ]
 
 MIDDLEWARE = [
