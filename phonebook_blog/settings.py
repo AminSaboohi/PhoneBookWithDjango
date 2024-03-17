c7d7e1c5414315ee66885e09ce9d0d71eb83263f
Amin Saboohi <aminsaboohi.eepd.gmail.com>
2024-03-17 14:13:25 +0330

    add ADMIN_PATH in local setting and import it to url, migrate project, create super user and enter to admin panel

--- phonebook_blog/settings.py
+++ phonebook_blog/settings.py
@@ -16,11 +16,8 @@ from .local_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-
 
 
 # Application definition
@@ -64,7 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'phonebook_blog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -74,7 +70,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -94,7 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -105,7 +99,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
